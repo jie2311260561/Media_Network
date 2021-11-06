131857ec2286dddde881a2da4606647c64a4ac92
jie2311260561 <2311260561@qq.com>
2021-11-06 23:11:33 +0800

    添加Windows底层支持SDL库

--- src/media-main/src/Media_app.c
+++ src/media-main/src/Media_app.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<string.h>
+#include "SDL2/SDL.h"
+
+#undef main
 
 
 int main(int argc, char const *argv[])
 {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
     if(argc == 1 || argc == 2){
         printf("Please input para \n");
         printf("input [-x] [value] \n");
@@ -19,5 +24,6 @@ int main(int argc, char const *argv[])
             }
         }
     }
+    SDL_Quit();
     return 0;
 }
