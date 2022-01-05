fe7af86a9121f5951a0426137c7d50c62dcc3d35
BedRock <2311260561@qq.com>
2022-01-05 23:11:26 +0800

    build4

---
+++ build/CMakeFiles/Media_Network.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Media_Network.dir/src/media-main/src/Media_app.c.o"
+  "Media_Network"
+  "Media_Network.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Media_Network.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
