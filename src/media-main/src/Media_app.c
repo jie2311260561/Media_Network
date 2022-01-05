47683613cb80263640c6325f187d11f8ed9af157
BedRock <2311260561@qq.com>
2022-01-05 18:26:29 +0800

    添加Linux编译还击

--- src/media-main/src/Media_app.c
+++ src/media-main/src/Media_app.c
@@ -1,10 +1,19 @@
+/*
+ * @Description: main入口
+ * @Version: 2.0
+ * @Autor: Bedrock
+ * @Date: 2021-11-25 20:50:54
+ * @LastEditors: Bedrock
+ * @LastEditTime: 2022-01-05 18:17:52
+ */
 #include<stdio.h>
 #include<string.h>
 #include "Media_main.h"
+#include "my_rtsp.h"
+
+#ifdef _COMPILE_WINDOWS_TEST_
 #include "win_media_video.h"
-
-
-
+#endif
 
 int main(int argc, char const *argv[])
 {
@@ -19,13 +28,20 @@ int main(int argc, char const *argv[])
             if(!strcmp(argv[2],"input")){
                 printf("server \n");
             }
+#ifdef _COMPILE_WINDOWS_TEST_
             else if(!strcmp(argv[2],"SDL")){
-
                 int res = win_media_window_show(0);
                 if(res < 0){
                     return 0;
                 }
             }
+#endif
+            else if (!(strcmp(argv[2],"rtsp")))
+            {
+                
+                return 0;
+            }
+            
             else{
                 printf("cherry-pick \n");
             }
