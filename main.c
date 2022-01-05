56718e99c638f90c35de4edb2603a32097febbfc
BedRock <2311260561@qq.com>
2022-01-05 23:20:14 +0800

    hello

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Bedrock
+ * @Date: 2022-01-05 23:17:04
+ * @LastEditors: Bedrock
+ * @LastEditTime: 2022-01-05 23:17:05
+ */
+
+
+#include <stdio.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello word!\n");
+    return 0;
+}
--- src/my_rtsp/my_rtsp.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_RTSP_H
-#define MY_RTSP_H
-
-
-int init_network_link();
-
-#endif
